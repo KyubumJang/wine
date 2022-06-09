1986b6f774d3eec2d2cee4e24727b8c357fdfd8b
Kyubum Jang <jkb2221@gmail.com>
2022-06-09 18:04:13 +0900

    feat: Add 컴포넌트 export index.js 추가

---
+++ src/components/atoms/index.js
@@ -0,0 +1,4 @@
+export { default as Box } from './Box/Box';
+export { default as Circle } from './Circle/Circle';
+export { default as Counter } from './Counter/Counter';
+export { default as Input } from './Input/Input';
